779e574e0859d7aed8696e57a9603652c9488bc1
MG <107312226+miguelache@users.noreply.github.com>
2023-11-27 04:09:57 -0300

    Cambio capacito.config
    Se cambia id y nombre de proyecto

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.duocuc.app',
   appName: 'ProyectoQr',
   webDir: 'www',
   server: {
